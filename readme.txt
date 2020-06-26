265a52f367074d6ae4e9386494f9ec6001175774
tehreem09 <tehreemfahim09@outlook.com>
2020-06-26 23:05:52 +0500

    branched created

--- readme.txt
+++ readme.txt
@@ -11,3 +11,24 @@ Documentation
 
 ###### Ignore this please
 - virtualenv --no-site-packages --distribute .env && source .env/bin/activate && pip install -r requirements.txt
+- https://www.truepeoplesearch.com/
+- https://www.whitepages.com/phone
+- https://www.spydialer.com/
+- http://phonesearcher.us/
+- https://www.nav.com/blog/how-to-get-a-business-phone-number-listed-33782/
+- A telephone directory and its content may be known by the colour of the paper it is printed on.
+
+        White pages generally indicates personal or alphabetic listings.
+        Yellow pages, golden pages, A2Z, or classified directory is usually a "business directory", where businesses are
+        listed alphabetically within each of many classifications (e.g., "lawyers"), almost always with paid advertising.
+        Grey pages, sometimes called a "reverse telephone directory", allowing subscriber details to be found for a
+        given number. Not available in all jurisdictions. (These listings are often published separately, in a city
+        directory, or under another name, for a price, and made available to commercial and government agencies.)
+         Business Listing Sites like JustBaazaar, JustDial, IndiaMart, Hotfrog, are nowadays very important to increase
+         online presence. Most of these websites offer a free business listing.
+
+    Other colors may have other meanings; for example, information on government agencies is often printed on blue pages
+    or green pages.[citation needed]
+- A reverse telephone directory is sorted by number, which can be looked up to give the name and address of the subscriber.
+- https://en.wikipedia.org/wiki/Silent_number
+- https://en.wikipedia.org/wiki/City_directory
--- snooper/numberhandler.py
+++ snooper/numberhandler.py
@@ -4,40 +4,78 @@ import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import timezone
+from _collections import defaultdict
 
 
-x = phonenumbers.parse("+442083661177", None)
-a = phonenumbers.parse("+92343286650", None)
-y = phonenumbers.parse("020 8366 1177", "GB")
-z = phonenumbers.parse("00 1 650 253 2222", "GB")  # as dialled from GB, not a GB number
-print(x, '\n', a, '\n', y, '\n', z)
-print('type:', type(x))
-print('....................................................')
-b = phonenumbers.parse("+120012301", None)
-check_false = 'possible' if phonenumbers.is_possible_number(b) else 'not possible'  # too few digits for USA
-validity_false = 'valid' if phonenumbers.is_valid_number(b) else 'not valid'
-print(b, check_false)
-print(b, validity_false)
-check_true = 'possible' if phonenumbers.is_possible_number(z) else 'not possible'  # too few digits for USA
-validity_true = 'valid' if phonenumbers.is_valid_number(z) else 'not valid'
-print(z, check_true)
-print(z, validity_true)
-print('....................................................')
-try:
-    c = phonenumbers.parse("02081234567", None)
-    d = phonenumbers.parse("gibberish", None)
-except phonenumbers.NumberParseException:
-    print("plz write correct phone number")
-else:
-    print('out of try except')
-print('....................................................')
-check_country = phonenumbers.parse('+923362314059', 'CH')
-print(check_country, 'Country: ', geocoder.description_for_number(check_country, 'en'))
-print('....................................................')
-ro_number = phonenumbers.parse("+40721234567", "RO")
-print(ro_number, 'Carrier: ', carrier.name_for_number(ro_number, "en"))
-print(check_country, 'Carrier: ', carrier.name_for_number(check_country, 'en'))
-print('.......................................................')
-gb_number = phonenumbers.parse("+447986123456", None)
-print(timezone.time_zones_for_number(gb_number))
-print(timezone.time_zones_for_number(check_country))
+class NumberRecon:
+    def __init__(self, **kwargs):
+        self.numbers = kwargs['numbers']
+        self.countries = kwargs['countries']
+        self.parsed_numbers = []
+        self.parsed_number = None
+        self.data_dict = defaultdict(list)
+        self.x = 0
+
+    def multiple_number_handler(self):
+        for number, country in list(zip(self.numbers, self.countries)):
+            self.x = self.x + 1
+            if self.number_validity_check(number, country):
+                self.parsed_numbers.append(self.parsed_number)
+                # parsed_number = phonenumbers.parse(number, country)
+                # number_origin_info = self.get_number_origin_data(number, parsed_number)
+            else:
+                print('[-] Following number is not valid and/or possible >', number)
+
+    def number_validity_check(self, number, country):
+        try:
+            print(self.x)
+            print(number, country)
+            self.parsed_number = phonenumbers.parse(number, country)
+            print(self.parsed_number)
+            phonenumbers.is_valid_number(self.parsed_number)
+            phonenumbers.is_possible_number(self.parsed_number)
+            return True
+        except phonenumbers.NumberParseException:
+            print('[-] Please provide the following phone number in International format or provide Region', number)
+            return False
+
+    def number_country_name(self, parsed_number):
+        return geocoder.description_for_number(parsed_number, 'en')
+
+    def number_timezone(self, parsed_number):
+        return timezone.time_zones_for_number(parsed_number)
+
+    def number_carrier(self, parsed_number):
+        return carrier.name_for_number(parsed_number, 'en')
+
+    def get_number_origin_data(self):
+        self.multiple_number_handler()
+        for p_number in self.parsed_numbers:
+            self.data_dict['number'].append(p_number)
+            self.data_dict['country'].append(self.number_country_name(p_number))
+            self.data_dict['time_zone'].append(self.number_timezone(p_number))
+            self.data_dict['carrier'].append(self.number_carrier(p_number))
+        # if self.data_dict:
+        #     print('blah')
+        #     return False
+        return self.data_dict
+
+
+numbers = ['+923362314059', '+923362314059', '+923362314059', '03362314059', '03362314059', '03362314059', '+923362314',
+           '+923362314', '+923362314', '03362314', '03362314', '03362314']
+countries = ['PK', None, 'GB', 'PK', None, 'GB', 'PK', None, 'GB', 'PK', None, 'GB']
+
+# numbers = ['+923362314059', '03362314059', '+923362314', '03362314']
+# countries = [None, 'PK', 'PK', 'PK']
+
+obj = NumberRecon(numbers=numbers, countries=countries)
+obj.get_number_origin_data()
+for key, value in obj.data_dict.items():
+    print(key, value)
+
+# check_country = phonenumbers.parse('+923362314059', 'CH')
+# print(check_country, 'Country: ', geocoder.description_for_number(check_country, 'en'))
+# gb_number = phonenumbers.parse("+447986123456", None)
+# print(timezone.time_zones_for_number(gb_number))
+# ro_number = phonenumbers.parse("+40721234567", "RO")
+# print(ro_number, 'Carrier: ', carrier.name_for_number(ro_number, "en"))
