57c4168ecdffe9d7d0ca7ca4b6c0012b9ae9b32f
tehreem09 <tehreemfahim09@outlook.com>
2020-08-11 20:59:49 +0500

    just a regular commit

--- snooper/__main__.py
+++ snooper/__main__.py
@@ -16,148 +16,20 @@ def arg_parser():
                                                                                           'email address owner\'s PII '
                                                                                           'information. '))
     parser.add_argument("-e", "--email", dest="email", type=str, help='email on which you want to perform snooping')
-    parser.add_argument("-n", "--name", dest="name", type=str, help='name on which you want to perform snooping')
-    parser.add_argument("-c", "--company", dest="my_company", type=str,
-                        help='company on which you want to perform snooping')
-    parser.add_argument("-o", "--occupation", dest="occ", type=str,
-                        help='occupation on which you want to perform snooping')
-    parser.add_argument("-s", "--surname", dest="surname", type=str,
-                        help='surname on which you want to perform snooping')
-    arg_logic(parser.parse_args())
+    parser.add_argument("-n", "--number", dest="number", type=int, help='name on which you want to perform snooping')
+    parser.add_argument('-f', '--file', dest='filename', type=argparse.FileType('r'), help='name or path to file of '
+                                                                                           'simple text file having '
+                                                                                           'number in each line.')
+    return parser.parse_args()
 
 
-def arg_logic(args_values):
-    if args_values.email:
-        print(args_values.email)
-        if args_values.name:
-            print(args_values.name)
-            if args_values.surname:
-                print(args_values.surname)
-                if args_values.occ:
-                    print(args_values.occ)
-                    if args_values.my_company:
-                        print(args_values.my_company)
-
-                elif args_values.my_company:
-                    print(args_values.my_company)
-            elif args_values.occ:
-                print(args_values.occ)
-                if args_values.my_company:
-                    print(args_values.my_company)
-                    if args_values.surname:
-                        print(args_values.surname)
-
-            elif args_values.my_company:
-                print(args_values.my_company)
-                if args_values.surname:
-                    print(args_values.surname)
-                    if args_values.occ:
-                        print(args_values.occ)
-
-        elif args_values.surname:
-            print(args_values.surname)
-            if args_values.name:
-                print(args_values.name)
-                if args_values.occ:
-                    print(args_values.occ)
-                    if args_values.my_company:
-                        print(args_values.my_company)
-
-                elif args_values.my_company:
-                    print(args_values.my_company)
-                    if args_values.occ:
-                        print(args_values.occ)
-
-            elif args_values.occ:
-                print(args_values.occ)
-                if args_values.my_company:
-                    print(args_values.my_company)
-                    if args_values.name:
-                        print(args_values.name)
-
-            elif args_values.my_company:
-                print(args_values.my_company)
-
-        elif args_values.occ:
-            print(args_values.occ)
-            if args_values.name:
-                print(args_values.name)
-                if args_values.my_company:
-                    print(args_values.my_company)
-                    if args_values.surname:
-                        print(args_values.surname)
-
-                elif args_values.surname:
-                    print(args_values.surname)
-                    if args_values.my_company:
-                        print(args_values.my_company)
-
-            elif args_values.surname:
-                print(args_values.surname)
-                if args_values.my_company:
-                    print(args_values.my_company)
-                    if args_values.name:
-                        print(args_values.name)
-                elif args_values.name:
-                    print(args_values.name)
-                    if args_values.my_company:
-                        print(args_values.my_company)
-            elif args_values.my_company:
-                print(args_values.my_company)
-                if args_values.surname:
-                    print(args_values.surname)
-                    if args_values.name:
-                        print(args_values.name)
-                elif args_values.name:
-                    print(args_values.name)
-                    if args_values.surname:
-                        print(args_values.surname)
-
-        elif args_values.my_company:
-            print(args_values.my_company)
-            if args_values.name:
-                print(args_values.name)
-                if args_values.occ:
-                    print(args_values.occ)
-                    if args_values.surname:
-                        print(args_values.surname)
-
-                elif args_values.surname:
-                    print(args_values.surname)
-                    if args_values.occ:
-                        print(args_values.occ)
-
-            elif args_values.surname:
-                print(args_values.surname)
-                if args_values.occ:
-                    print(args_values.occ)
-                    if args_values.name:
-                        print(args_values.name)
-                elif args_values.name:
-                    print(args_values.name)
-                    if args_values.occ:
-                        print(args_values.occ)
-            elif args_values.occ:
-                print(args_values.occ)
-                if args_values.surname:
-                    print(args_values.surname)
-                    if args_values.name:
-                        print(args_values.name)
-                elif args_values.name:
-                    print(args_values.name)
-                    if args_values.surname:
-                        print(args_values.surname)
-
-    else:
-        print(' [-] please enter email')
-
-
-def db_check():
+def db_check(numbers):
     pass
 
 
-def number_basic_info(numbers, countries):
-
+def number_basic_info(numbers):
+    x = len(numbers)
+    countries = [None] * x
     obj = numberhandler.NumberRecon(numbers=numbers, countries=countries)
     number_basic_data_dict = obj.get_number_origin_data()
     with open('basic_number_info.json', 'w') as fp:
@@ -175,34 +47,31 @@ def live_search():
     process.crawl(lifetechcrawlerpy.LifetechCrawler)
     process.start()
     ec2tos3.upload_file_to_s3bucket('', 'lifetech_rawdata.json', 'lifetechdata.json')
-    pass
 
 
-def file_handler():
-
+def file_handler(filename):
+    file = open(filename, 'r')
+    numbers_list = [line.strip('\n') + ',' for line in file.readlines()]
+    file.close()
+    return numbers_list
 
 
 def main():
-    arg_parser()
-    if db_check():
-        pass
-    else:
-        numbers = ['+923362314059', '+923362314059', '+61449765241', '+12017237150']
-        countries = ['PK', None, None, None]
-        number_basic_info(numbers, countries)
-        # live_search()
-        numbers = ['+923362314059', '+923362314059', '+61449765241', '+12017237150']
-        x = len(numbers)
-        countries = [None] * x
-        obj = numberhandler.NumberRecon(numbers=numbers, countries=countries)
-        number_basic_data_dict = obj.get_number_origin_data()
-        with open('basic_number_info.json', 'w') as fp:
-            json.dump(number_basic_data_dict, fp, indent=4)
-        fp.close()
-
-    file = open('demo_numbers.txt', 'r')
-    numbers_list = [line.strip('\n')+',' for line in file.readlines()]
-    print(numbers_list)
+    arg_values = arg_parser()
+    if arg_values.number:
+        numbers = list(arg_values.number)
+        if db_check(numbers):
+            pass
+        else:
+            number_basic_info(numbers)
+            live_search()
+    elif arg_values.file:
+        numbers = file_handler(arg_values.filename)
+        if db_check(numbers):
+            pass
+        else:
+            number_basic_info(numbers)
+            live_search()
 
 
 if __name__ == '__main__':
