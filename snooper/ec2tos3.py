93a30cc1320e8c481ee3c59b672fc697beea3cad
tehreem09 <42211530+tehreem09@users.noreply.github.com>
2020-11-25 11:56:47 +0500

    Update ec2tos3.py

--- snooper/ec2tos3.py
+++ snooper/ec2tos3.py
@@ -6,6 +6,7 @@ def upload_file_to_s3bucket(filepath, datafilename, newname):
     s3 = boto3.resource('s3')
 
     # Upload a new file
+    print("copying " +datafilename+ " to s3 bucket's destination rawdata/"+newname)
     data = open(filepath+datafilename, 'rb')
     s3.Bucket('aws-scraper-bucket').put_object(Key='rawdata/'+newname, Body=data)
     data.close()
