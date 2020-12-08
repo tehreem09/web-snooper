bd02ef0aa38881321da8dc76b28560a7381b3841
AdeenaShabbir <44442025+AdeenaShabbir@users.noreply.github.com>
2020-12-08 15:29:30 +0500

    Update db_hadler.py

--- snooper/db_hadler.py
+++ snooper/db_hadler.py
@@ -45,12 +45,27 @@ def search_records():
 
         main_dbt_handler(number, result)
 
+# def basic_info_merger(dict):
+#     with open ('basic_number_info.json', 'r') as basic_num_info:
+#         num_info = json.load(basic_num_info)
+#     for data in num_info:
+#         # print(data)
+#         number = str(data.get('number'))[2:-2]
+#         # print(number)
+#         number2 = '+92'+ dict['number']
+#         # print ("with" + number2)
+#         if ('+92'+dict['number'])==number:
+#             print("number matcheddd")
+#             new_dict = merge_found_records(dict, data)
+#             return new_dict
+#     return dict
+
 def basic_info_merger(dict):
     with open ('basic_number_info.json', 'r') as basic_num_info:
         num_info = json.load(basic_num_info)
     for data in num_info:
         # print(data)
-        number = str(data.get('number'))[2:-2]
+        number = data.get('number')
         # print(number)
         number2 = '+92'+ dict['number']
         # print ("with" + number2)
